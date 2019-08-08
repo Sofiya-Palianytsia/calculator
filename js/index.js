05c4a690fd13106c225b5d19025a0f8aee668ffd
Sofiya-Palianytsia <sofiya.palianytsia@gmail.com>
2019-08-08 07:59:49 +0000

    clear

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ const display = document.querySelector('.calculator .display');
 document
     .querySelectorAll('.digits button')
     .forEach(digit => digit.addEventListener('click', digitPressed));
-
 function digitPressed(ev) {
     display.value += ev.target.innerText;
 }
@@ -11,7 +10,6 @@ function digitPressed(ev) {
 document
     .querySelectorAll('.opers button')
     .forEach(oper => oper.addEventListener('click', operPressed));
-
 function operPressed(ev) {
     display.value += ev.target.innerText;
 }
@@ -20,3 +18,16 @@ document.querySelector('.equal').addEventListener('click', equalPressed);
 function equalPressed () {
     display.value = eval(display.value);
 }
+
+document.querySelector('.clear').addEventListener('click', clearPressed);
+function clearPressed (ev) {
+   display.value = '';
+}
+
+/*else if (btnText === 'CE') { // delete one character
+            input = input.slice(0, input.length - 1);*/
+
+/*if (input.indexOf('.') === -1 || dotFlag) { // only one dot is allowed
+                input += '.';
+                dotFlag = false;
+            }*/
